60e33e7609a7ce1d3b1169a2791c7c75f0f504d5
ccozzi13 <61848659+ccozzi13@users.noreply.github.com>
2024-11-21 17:24:45 -0700

    Update sales_summary.py

--- sales_summary/sales_summary.py
+++ sales_summary/sales_summary.py
@@ -36,7 +36,7 @@ def get_toast_access_token(toastAPIHost):
     
     secret_block = Secret.load("analytics-creds")
 
-    creds = json.load(secret_block)
+    creds = json.load(secret_block.get())
 
     ToastUserId = creds['toastAnalyticsAPIUserProd']
     ToastSecret = creds['toastAnalyticsAPISecretProd']
